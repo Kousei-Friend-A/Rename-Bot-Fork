f532a07be285ee723cc24098c7556109d294ff19
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2023-01-24 21:35:16 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "5700920410:AAEk8j5NZb6wiIVuu3-NS6AL243bl57EpvU"
 
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = "4a05481a5da2d66f801acffc4ca5ee4b"
+    API_HASH = 17945796
     # Get these values from my.telegram.org
 
     # Ban Unwanted Members..
@@ -23,5 +23,5 @@ class Config(object):
     CHUNK_SIZE = 128
 
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = "mongodb+srv://manu911:manu911@cluster0.frtlbqt.mongodb.net/?retryWrites=true&w=majority"
 
